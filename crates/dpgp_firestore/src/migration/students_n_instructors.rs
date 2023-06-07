5d64e21b983ba71faf63a15222d009de07989b68
Mushogenshin <mushogenshin@gmail.com>
2023-06-07 03:23:55 -0500

    sqlite fetch_all

--- crates/dpgp_firestore/src/migration/students_n_instructors.rs
+++ crates/dpgp_firestore/src/migration/students_n_instructors.rs
@@ -11,6 +11,17 @@ mod tests {
         name: String,
         #[sqlx(rename = "Email")]
         email: String,
+        #[sqlx(rename = "Class")]
+        class_str: String,
+
+        #[sqlx(rename = "Discord_UserID")]
+        discord_id: Option<i64>,
+        #[sqlx(rename = "Discord_Username")]
+        /// This can be `EMPTY` instead of `NULL`.
+        discord_username: String,
+        #[sqlx(rename = "Facebook")]
+        /// This can be `EMPTY` instead of `NULL`.
+        facebook: String,
     }
 
     async fn connect_to_file() -> AnyResult<SqlitePool> {
@@ -21,18 +32,17 @@ mod tests {
         Ok(pool)
     }
 
-    async fn list_students(pool: &SqlitePool) -> AnyResult<Vec<User>> {
+    async fn list_students(pool: &SqlitePool) -> AnyResult<Vec<Student>> {
         let recs = sqlx::query_as::<_, Student>(
             r#"
-SELECT Name, Email
+SELECT Name, Email, Class, Discord_UserID, Discord_Username, Facebook
 FROM Students
     "#,
         )
         .fetch_all(pool)
-        .await?;
+        .await;
 
-        eprintln!("{:?}", recs);
-        Ok(vec![])
+        Ok(recs?)
     }
 
     #[tokio::test]
@@ -40,6 +50,8 @@ FROM Students
         let db = connect_to_file().await?;
 
         let students = list_students(&db).await?;
+
+        eprintln!("{:#?}", students);
 
         Ok(())
     }
