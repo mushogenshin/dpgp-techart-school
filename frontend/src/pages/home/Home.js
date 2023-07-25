1c03f1d96274ee23c05f6f3e8def93478f56f88b
Mushogenshin <mushogenshin@gmail.com>
2023-07-25 12:30:00 -0500

    manifesto

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -9,7 +9,46 @@ export default function Home() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      Welcome!
+      Bắt đầu nguyên thuỷ với các lớp dạy Anatomy về cơ thể người, đến nay "Dẫu
+      Phải Giải Phẫu" đã mở rộng sang rất nhiều bộ môn khác nhau, và tuy gom
+      chung dưới cái tên bao trùm "Technical Art", các lớp Art của DPGP xưa nay
+      vẫn tập trung vào khía cạnh thẩm mỹ và kỹ năng hội hoạ căn bản, không khác
+      gì các chương trình mỹ thuật chính thống.
+      <br />
+      <br />
+      DPGP tin rằng bên cạnh việc vẽ là đam mê, là sở thích, thì vẽ còn có thể
+      là giao tiếp: trao đổi giữa tác giả và khán giả, và trên hết, vẽ là tư
+      duy: là tổng kết những gì ta hiểu về sự vật, hiện tượng, và thế giới; và
+      khát khao lẫn tiềm tàng sáng tạo của một người hoạ sĩ thời hiện đại còn có
+      thể biểu đạt qua rất nhiều hình thái khác: bao gồm những bộ môn nằm trong
+      chữ "Tech" còn lại của TechArt.
+      <br />
+      <br />
+      Đó là lý do mà xuất thân từ bộ môn giải phẫu, cũng như cách nhân loại từ
+      đó đã có thời kỳ Phục Hưng và Cách mạng Khoa học và Công nghiệp, nay "Dẫu
+      Phải Giải Phẫu" cảm thấy một sự phát triễn vô cùng tự nhiên: sau hành
+      trình 8 năm với hơn 500 học viên, chúng ta sẽ không chỉ dừng lại ở
+      Anatomy, mà cả một chân trời (sau khi hiểu rõ trước tiên về bộ máy cơ thể
+      con người) nay đã mở ra: không chỉ là Animation, Rigging, lập trình, nhưng
+      là khả năng hiểu tường tận gần như mọi thứ mà chúng ta mong muốn, là óc tư
+      duy phân tích, là óc tổng hợp vấn đề.
+      <br />
+      <br />
+      "Dẫu Phải Giải Phẫu" từ đó thân mời các bạn cùng dấn bước, vẫn thoải mái
+      hăng say với nghề vẽ, nhưng cũng không hề sợ hãi những bộ môn kỹ thuật
+      khác, bởi vì vẽ từ đầu đã là một cách để thông qua đó ta hiểu thế giới, và
+      người đam mê vẽ là người đã được "thiên phú" cho trí tò mò, lòng say mê
+      tìm hiểu, là những phẩm chất cực kì quý báu trong thời đại hôm nay.
+      <br />
+      <br />
+      Chúng ta không cần sợ mang danh "Artist không ra Artist" ngay cả khi theo
+      đuổi TechArt. Cũng như ta không việc gì phải xấu hổ khi chọn theo học
+      "những lớp có cảm giác quá kỹ thuật" để phục vụ cho hành trình nghệ thuật
+      của mình cả. Trái lại, những người còn giữ ám ảnh "Tech versus Art" mới
+      chính là những người có tư duy hạn hẹp. Sử dụng nhuần nhuyễn kỹ thuật để
+      chuyên chở cảm xúc, đó chẳng phải là điều các bậc thầy trong ẩm thực, thời
+      trang, hội hoạ, điêu khắc, kiến trúc, thơ ca, âm nhạc, kịch nghệ, nhiếp
+      ảnh, điện ảnh, VFX, comics, v.v. đã làm từ bấy lâu hay sao?
     </div>
   );
 }
