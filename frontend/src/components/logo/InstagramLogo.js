af86808aba2e52fb28f99f73ed7ff92846406eeb
Mushogenshin <mushogenshin@gmail.com>
2023-08-25 11:22:50 +0700

    social logo

---
+++ frontend/src/components/logo/InstagramLogo.js
@@ -0,0 +1,10 @@
+export default function InstagramLogo() {
+  return (
+    <img
+      src="https://static.xx.fbcdn.net/rsrc.php/v3/yx/r/tBxa1IFcTQH.png"
+      alt="Instagram"
+      width={24}
+      height={24}
+    />
+  );
+}
--- frontend/src/pages/about/About.js
+++ frontend/src/pages/about/About.js
@@ -1,4 +1,7 @@
 import styles from "./About.module.css";
+import InstagramLogo from "../../components/logo/InstagramLogo";
+import ArtStationLogo from "../../components/logo/ArtStationLogo";
+import LinkedInLogo from "../../components/logo/LinkedInLogo";
 
 export default function About() {
   return (
@@ -11,18 +14,16 @@ export default function About() {
             target="_blank"
             rel="noreferrer noopener"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              data-supported-dps="24x24"
-              fill="currentColor"
-              class="mercado-match flipthis-highlight"
-              width="24"
-              height="24"
-              focusable="false"
-            >
-              <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
-            </svg>
+            <LinkedInLogo />
+          </a>
+        </span>{" "}
+        <span>
+          <a
+            href="https://www.artstation.com/mushogenshin"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <ArtStationLogo />
           </a>
         </span>
       </h1>
@@ -42,12 +43,7 @@ export default function About() {
             target="_blank"
             rel="noreferrer noopener"
           >
-            <img
-              src="https://static.xx.fbcdn.net/rsrc.php/v3/yx/r/tBxa1IFcTQH.png"
-              alt="Instagram"
-              width={24}
-              height={24}
-            />
+            <InstagramLogo />
           </a>
         </span>
       </h1>
@@ -109,22 +105,6 @@ export default function About() {
       <p>Nguyên.</p>
 
       <hr></hr>
-      <h1>Phùng Nhật Huy</h1>
-      <p>...</p>
-
-      <hr></hr>
-      <h1>Hoàng Đức Phúc</h1>
-      <p>...</p>
-
-      <hr></hr>
-      <h1>Nguyễn Minh Sơn</h1>
-      <p>...</p>
-
-      <hr></hr>
-      <h1>Jacob Võ</h1>
-      <p>...</p>
-
-      <hr></hr>
       <h1>kwon</h1>
       <p>...</p>
 
@@ -133,8 +113,24 @@ export default function About() {
       <p>...</p>
 
       <hr></hr>
-      <h1>Vũ Phan Ngọc Minh</h1>
+      <h1>Phùng Nhật Huy</h1>
       <p>...</p>
+
+      <hr></hr>
+      <h1>Hoàng Đức Phúc</h1>
+      <p>...</p>
+
+      {/* <hr></hr>
+      <h1>Nguyễn Minh Sơn</h1>
+      <p>...</p> */}
+
+      <hr></hr>
+      <h1>Jacob Võ</h1>
+      <p>...</p>
+
+      {/* <hr></hr>
+      <h1>Vũ Phan Ngọc Minh</h1>
+      <p>...</p> */}
     </div>
   );
 }
