7f36e538531cbdc2ab62fcc027515074cbdffd73
Mushogenshin <mushogenshin@gmail.com>
2023-08-27 09:59:14 +0700

    usersCount component

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -6,20 +6,12 @@ import styles from "./Navbar.module.css";
 
 export default function Navbar() {
   const { user, elevatedRole } = useAuthContext();
-  const { usersCount } = useUsersContext();
 
   return (
     <nav className={styles.navbar}>
       <ul>
         <li className={styles.title}>
-          <Link to="/">DPGP TechArt</Link>{" "}
-          {usersCount && (
-            <span className={styles.circle}>
-              <span className={styles["users-count"]} title="Users Count">
-                {usersCount}
-              </span>
-            </span>
-          )}
+          <Link to="/">DPGP TechArt</Link> <UserCount />
         </li>
         {elevatedRole && (
           <Link to="/admin" title="Admin" className={styles.circle}>
@@ -43,5 +35,22 @@ export default function Navbar() {
         )}
       </ul>
     </nav>
+  );
+}
+
+function UserCount() {
+  const { usersCount } = useUsersContext();
+  return (
+    usersCount && (
+      <span>
+        <span style={{ fontSize: "1.7em" }}>| </span>
+        Users Count:{" "}
+        <span className={styles.circle}>
+          <span className={styles["users-count"]} title="Users Count">
+            {usersCount}
+          </span>
+        </span>
+      </span>
+    )
   );
 }
