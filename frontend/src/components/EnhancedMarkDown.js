c1d3696e9f40729559f1a694d52119d53fa89b4a
Mushogenshin <mushogenshin@gmail.com>
2023-09-14 16:21:11 +0700

    use ReactMarkdown linkTarget

--- frontend/src/components/EnhancedMarkDown.js
+++ frontend/src/components/EnhancedMarkDown.js
@@ -1,17 +1,24 @@
 import ReactMarkdown from "react-markdown";
 import CodeBlock from "./CodeBlock";
 
+/**
+ * A component that renders Markdown with syntax highlighting and
+ * anchor opened in a new tab.
+ *
+ * @param {string} children - The Markdown content to render.
+ */
 export default function EnhancedMarkDown({ children }) {
   return (
     <ReactMarkdown
       children={children}
+      linkTarget="_blank"
       components={{
         code({ ...props }) {
           return CodeBlock({ ...props });
         },
-        a({ ...props }) {
-          return <a {...props} target="_blank" rel="noopener noreferrer" />;
-        },
+        // a({ ...props }) {
+        //   return <a {...props} target="_blank" rel="noopener noreferrer" />;
+        // },
       }}
     />
   );
