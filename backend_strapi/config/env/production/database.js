ab239c49085123ed4e11acbe06e6ff55593496ed
Mushogenshin <mushogenshin@gmail.com>
2024-04-24 14:53:29 -0500

    Strapi production DB

--- backend_strapi/config/env/production/database.js
+++ backend_strapi/config/env/production/database.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
+      database: env("DATABASE_NAME", "dpgp_strapi"),
       user: env("DATABASE_USERNAME", "strapi"),
-      password: env("DATABASE_PASSWORD", "strapi"),
+      password: env("DATABASE_PASSWORD", "SXuDFq0XJHdBuvZ2yRlex"),
       schema: env("DATABASE_SCHEMA", "public"), // Not required
       ssl: {
         rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
