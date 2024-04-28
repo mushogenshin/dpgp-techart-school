57314fa99902122d90c49513848f215fe61b4e52
Mushogenshin <mushogenshin@gmail.com>
2024-04-28 10:09:21 -0500

    strapi transfer to URL

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,6 +1,5 @@
 import { useFetchPublicPage } from "../../hooks/firestore/useFetchPublicPage";
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 
 import styles from "./Home.module.css";
 
