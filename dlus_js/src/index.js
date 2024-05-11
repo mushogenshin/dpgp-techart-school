393955f3bc3121c71b911646c301772ca961ded9
Mushogenshin <mushogenshin@gmail.com>
2024-05-11 13:50:22 -0500

    use NotUnderCtrl commandkit

--- dlus_js/src/index.js
+++ dlus_js/src/index.js
@@ -1,5 +1,12 @@
-require("dotenv").config();
-const { Client, IntentsBitField } = require("discord.js");
+import 'dotenv/config';
+
+import { Client, IntentsBitField } from 'discord.js';
+import { CommandKit } from 'commandkit';
+
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const client = new Client({
   intents: [
@@ -7,12 +14,13 @@ const client = new Client({
     IntentsBitField.Flags.GuildMembers,
     IntentsBitField.Flags.GuildMessages,
     IntentsBitField.Flags.MessageContent,
-    IntentsBitField.Flags.DirectMessages,
   ],
 });
 
-client.on("ready", (c) => {
-  console.log(`🥸 Logged in as ${c.user.tag}`);
+new CommandKit({
+  client,
+  eventsPath: join(__dirname, 'events'),
+  commandsPath: join(__dirname, 'commands'),
 });
 
-client.login(process.env.DISCORD_BOT_TOKEN);
+client.login(process.env.TOKEN);
