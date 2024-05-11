810d30244cd2d49659ef6e5294fdab6405ada675
Mushogenshin <mushogenshin@gmail.com>
2024-05-10 19:00:15 -0500

    token from dotenv

--- dlus_js/src/index.js
+++ dlus_js/src/index.js
@@ -1,1 +1,18 @@
+require("dotenv").config();
 const { Client, IntentsBitField } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+    IntentsBitField.Flags.DirectMessages,
+  ],
+});
+
+client.on("ready", (c) => {
+  console.log(`🥸 Logged in as ${c.user.tag}`);
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
