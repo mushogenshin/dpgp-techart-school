6ead1bf9439c02d56359af3664bac1d93d504e63
Mushogenshin <mushogenshin@gmail.com>
2024-06-17 12:16:21 -0500

    WIP: palettes and hotkeys

--- frontend_react/src/pages/dfs/Poi.js
+++ frontend_react/src/pages/dfs/Poi.js
@@ -1,6 +1,52 @@
 import React from "react";
+import ReactMarkdown from "react-markdown";
 import styles from "./Poi.module.css";
 
 export default function Poi() {
-  return <div className={styles.poi}>Cài tuỳ chỉnh của Hoan cho ZBrush</div>;
+  const markdownContent = `
+# Cài tuỳ chỉnh của Hoan cho ZBrush
+
+---
+## 🎨 Giới thiệu 4 palette (A, B, C, D)
+abc  
+abc  
+abc  
+abc  
+abc  
+abc  
+
+## ⌨️ Phím tắt
+def  
+def  
+def  
+def  
+def  
+def  
+def  
+
+## 🙀 Cách cài vào máy (thủ công)
+ghi  
+ghi  
+ghi  
+ghi  
+ghi  
+ghi  
+ghi  
+
+## 🤖 Cách cài vào máy (nhanh)
+jkl  
+jkl  
+jkl  
+jkl  
+jkl  
+jkl  
+jkl  
+jkl  
+  `;
+
+  return (
+    <div className={styles.poi}>
+      <ReactMarkdown>{markdownContent}</ReactMarkdown>
+    </div>
+  );
 }
