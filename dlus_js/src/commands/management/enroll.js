557cd6c67ad27dd2a98cbe96b975ad167865ddae
Mushogenshin <mushogenshin@gmail.com>
2025-01-03 19:44:46 -0600

    WIP: send back screenshot attachment

--- dlus_js/src/commands/management/enroll.js
+++ dlus_js/src/commands/management/enroll.js
@@ -1,31 +1,28 @@
 // import { getClassData } from "../../firebase_config";
 
-const {
-  ApplicationCommandOptionType,
-  AttachmentBuilder,
-} = require("discord.js");
+const { ApplicationCommandOptionType } = require("discord.js");
 
 /** @type {import('commandkit').CommandData}  */
 export const data = {
   name: "register",
   description: "Báo nộp tiền học",
   options: [
-    // {
-    //   name: "email",
-    //   description: "Email dùng để access khoá học",
-    //   type: ApplicationCommandOptionType.String,
-    //   required: true,
-    // },
-    // {
-    //   name: "product",
-    //   description: "Mã số sản phẩm",
-    //   type: ApplicationCommandOptionType.Integer,
-    //   required: true,
-    // },
     {
       name: "screenshot",
       description: "Screenshot giao dịch",
       type: ApplicationCommandOptionType.Attachment,
+      required: true,
+    },
+    {
+      name: "email",
+      description: "Email dùng để access khoá học",
+      type: ApplicationCommandOptionType.String,
+      required: true,
+    },
+    {
+      name: "product",
+      description: "Mã số sản phẩm",
+      type: ApplicationCommandOptionType.Integer,
       required: true,
     },
   ],
@@ -35,27 +32,16 @@ export const data = {
  * @param {import('commandkit').SlashCommandProps} param0
  */
 export const run = async ({ interaction, client, _handler }) => {
-  // const email = interaction.options.get("email");
-  // const product = interaction.options.get("product");
+  const email = interaction.options.get("email");
+  const product = interaction.options.get("product");
+
+  // https://discord.js.org/docs/packages/discord.js/main/Attachment:Class
   const screenshot = interaction.options.get("screenshot");
 
-  const formattedScreenshot = screenshot
-    ? JSON.stringify(screenshot, null, 2) // Pretty-print JSON with 2-space indentation
-    : "No screenshot data found";
-
-  // // Create an attachment from the provided screenshot
-  // const attachment = new AttachmentBuilder(screenshot.url, {
-  //   name: screenshot.name,
-  // });
-
-  // interaction.reply(
-  //   `TODO: create ticket for ${email.value} with product ${product.value} and screenshot ${screenshot.value}`
-  // );
-  // Send a message with the attachment
+  // Send back a message with the same attachment
   await interaction.reply({
-    // content: `Email: ${email}\nProduct: ${product}`,
-    content: `Screenshot: ${formattedScreenshot}`,
-    // files: [attachment],
+    content: `TODO: create ticket for email: ${email.value}\nProduct: ${product.value}`,
+    files: [screenshot.attachment],
   });
 };
 
