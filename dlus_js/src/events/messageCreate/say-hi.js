0841a43deaf07ae3bd7c95553e89a83c732d4741
Mushogenshin <mushogenshin@gmail.com>
2025-01-05 17:01:17 -0600

    disable reply to Hey

--- dlus_js/src/events/messageCreate/say-hi.js
+++ dlus_js/src/events/messageCreate/say-hi.js
@@ -1,5 +1,5 @@
-export default function (message, client, handler) {
-  if (message.content === "hey") {
-    message.reply("Hi!");
-  }
+export default function (message, _client, _handler) {
+  // if (message.content === "hey") {
+  //   message.reply("Hi!");
+  // }
 }
