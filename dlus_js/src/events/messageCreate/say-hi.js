9ba017b734bdf0fbfe0a8ac4f43e5fca875f13a9
Mushogenshin <mushogenshin@gmail.com>
2025-01-08 10:38:14 -0600

    say-hi with rock-paper-scissors game and async awaitMessages

--- dlus_js/src/events/messageCreate/say-hi.js
+++ dlus_js/src/events/messageCreate/say-hi.js
@@ -1,5 +1,14 @@
-export default function (message, _client, _handler) {
-  // if (message.content === "hey") {
-  //   message.reply("Hi!");
-  // }
+export default async function (message, client, handler) {
+  if (message.content.toLowerCase() === "!rps") {
+    await message.channel.send("Input your choice!");
+    const filter = (m) =>
+      ["rock", "paper", "scissors"].includes(m.content.toLowerCase());
+    const msg = await message.channel.awaitMessages({
+      filter,
+      max: 1,
+      time: 30000,
+      errors: ["time"],
+    });
+    await message.channel.send(`You chose ${msg.first().content}`);
+  }
 }
