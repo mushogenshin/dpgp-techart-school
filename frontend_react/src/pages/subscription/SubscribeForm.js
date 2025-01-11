c85edad80a7658dd9cdf6442ada3cf7a757a27fa
Mushogenshin <mushogenshin@gmail.com>
2025-01-11 11:50:38 -0600

    subscribe form CSS for inline

--- frontend_react/src/pages/subscription/SubscribeForm.js
+++ frontend_react/src/pages/subscription/SubscribeForm.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useAuthContext } from "../../hooks/auth/useAuthContext";
 
 import styles from "./Subscription.module.css";
@@ -7,9 +8,52 @@ import styles from "./Subscription.module.css";
  */
 export default function SubscribeForm() {
   const { user } = useAuthContext();
+  const [email, setEmail] = useState("");
+  const [isEmailValid, setIsEmailValid] = useState(false);
+
+  const isPending = false; // TODO
+
+  const handleEmailChange = (event) => {
+    const emailValue = event.target.value;
+    setEmail(emailValue);
+    setIsEmailValid(
+      emailValue.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i) !== null
+    );
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+    console.log("Email submitted:", email);
+  };
+
   if (user) {
     return null;
   }
 
-  return <div className={styles.form}>TODO: Subscribe Form</div>;
+  return (
+    <div className={styles.form}>
+      <form onSubmit={handleSubmit} className={styles.inlineForm}>
+        <label htmlFor="email" className={styles.inlineLabel}>
+          Email:
+        </label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={handleEmailChange}
+          required
+          placeholder="Nhập email"
+          className={styles.inlineInput}
+        />
+        <button
+          type="submit"
+          className={`btn ${styles.inlineButton}`}
+          disabled={isPending || !isEmailValid}
+        >
+          {isPending ? "Đang request..." : "Subscribe"}
+        </button>
+      </form>
+    </div>
+  );
 }
