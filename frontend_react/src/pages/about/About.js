09829225fabb4196346580805b6877819508f559
Mushogenshin <mushogenshin@gmail.com>
2025-01-10 21:37:54 -0600

    WIP: disable Manifesto in About page

--- frontend_react/src/pages/about/About.js
+++ frontend_react/src/pages/about/About.js
@@ -5,25 +5,29 @@ import SubscribeForm from "../subscription/SubscribeForm";
 import styles from "./About.module.css";
 
 export default function About() {
+  return (
+    <div>
+      {/* <Manifesto /> */}
+      <div>
+        <SubscribeForm />
+      </div>
+    </div>
+  );
+}
+
+function Manifesto() {
   const { pageData, error, isPending } = useFetchPublicPage("about");
   const blocks = pageData?.versions["1"]?.blocks;
 
   return (
-    <div>
-      <div className={styles.home}>
-        {error && <h2>😳 {error}</h2>}
-        {isPending ? (
-          <p>Đợi xíu nha 😙...</p>
-        ) : (
-          blocks &&
-          blocks.map((block, index) => (
-            <ContentBlock key={index} block={block} />
-          ))
-        )}
-      </div>
-      <div>
-        <SubscribeForm />
-      </div>
+    <div className={styles.home}>
+      {error && <h2>😳 {error}</h2>}
+      {isPending ? (
+        <p>Đợi xíu nha 😙...</p>
+      ) : (
+        blocks &&
+        blocks.map((block, index) => <ContentBlock key={index} block={block} />)
+      )}
     </div>
   );
 }
