d9f44e1b069b99a584f1810d257b3c13bd9f1916
Hoan Nguyen <info@mushogenshin.com>
2025-01-27 17:38:07 -0600

    tried to silence Vimeo 403 error but didn't work

--- frontend_react/src/components/ContentBlock/Vimeo.js
+++ frontend_react/src/components/ContentBlock/Vimeo.js
@@ -1,23 +1,62 @@
+// import { useEffect, useRef } from "react";
+
 export default function Vimeo({ id }) {
+  // const iframeRef = useRef(null);
+
+  // useEffect(() => {
+  //   const iframe = iframeRef.current;
+
+  //   const handleMessage = (event) => {
+  //     if (event.origin !== "https://player.vimeo.com") return;
+  //     if (
+  //       event.data &&
+  //       event.data.event === "error" &&
+  //       event.data.data &&
+  //       event.data.data.message &&
+  //       event.data.data.message.includes("privacy")
+  //     ) {
+  //       iframe.style.display = "none";
+  //       iframe.addEventListener("error", (e) => e.preventDefault());
+  //     }
+  //   };
+
+  //   const handleLoad = (event) => {
+  //     console.log(`Vimeo iframe loaded: ${event.target.src}`);
+  //   };
+
+  //   window.addEventListener("message", handleMessage);
+  //   if (iframe) {
+  //     iframe.addEventListener("load", handleLoad);
+  //   }
+
+  //   return () => {
+  //     window.removeEventListener("message", handleMessage);
+  //     if (iframe) {
+  //       iframe.removeEventListener("load", handleLoad);
+  //     }
+  //   };
+  // }, []);
+
   return (
     <div
       // Vimeo embed styling
       style={{ padding: "56.25% 0 0 0", position: "relative" }}
     >
       <iframe
+        // ref={iframeRef}
         title="Vimeo video player"
         src={`https://player.vimeo.com/video/${id}?badge=0&amp;autopause=0&amp`}
         width="640"
         height="360"
-        frameBorder="0"
-        allow="autoplay; fullscreen; picture-in-picture"
         style={{
+          border: "none",
           position: "absolute",
           top: "0",
           left: "0",
           width: "100%",
           height: "100%",
         }}
+        allow="autoplay; fullscreen; picture-in-picture"
       ></iframe>
     </div>
   );
